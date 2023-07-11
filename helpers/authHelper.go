b6ec8aa4de5508ac3e0e9ffcf19d9f64b91dc51b
mbrunoon <contato@marcosbruno.com>
2023-07-11 08:58:30 +0100

    feat: add AuthHelper

---
+++ helpers/authHelper.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) error {
+	userType := c.GetString("user_type")
+	var err error = nil
+	if userType != role {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) error {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	var err error = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+
+	CheckUserType(c, userType)
+	return err
+
+}
