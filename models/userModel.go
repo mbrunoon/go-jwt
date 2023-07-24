c8124c87af641f1eec32f2799b1d2a19025b509c
mbrunoon <contato@marcosbruno.com>
2023-07-24 08:29:08 +0100

    fix: string type as pointers

--- models/userModel.go
+++ models/userModel.go
@@ -8,14 +8,14 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `bson: _id`
-	FirstName    string             `json:first_name validate:"required, min=2, max=100"`
-	LastName     string             `json:"last_name" validate:"required, min=2, max=2"`
-	Password     string             `json:"password" validate:"required, min=6"`
-	Email        string             `json:"email" validate:"email, required"`
-	Phone        string             `json:"phone" validate:"required"`
-	Token        string             `json:"json_token" validate:"required"`
-	UserType     string             `json:"user_type" validate:"required, eq=ADMIN|eq=USER"`
-	RefreshToken string             `json:"refresh_token`
+	FirstName    *string            `json:"first_name" validate:"required,min=2,max=100"`
+	LastName     *string            `json:"last_name" validate:"required,min=2,max=100"`
+	Password     *string            `json:"password" validate:"required,min=6"`
+	Email        *string            `json:"email" validate:"email,required"`
+	Phone        *string            `json:"phone" validate:"required"`
+	Token        *string            `json:"json_token"`
+	UserType     *string            `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
+	RefreshToken *string            `json:"refresh_token`
 	CreatedAt    time.Time          `json:"created_at"`
 	UpdatedAt    time.Time          `json:"updated_at"`
 	UserID       string             `json:"user_id"`
